d44c28d3705724f94d0ad3bcff2198c46133cb70
ADRDra <alexisHetic>
2016-03-21 19:58:13 +0100

    Connect to SQS service

---
+++ index.js
@@ -0,0 +1,25 @@
+import Consumer from 'sqs-consumer';
+import AWS from 'aws-sdk';
+import getAWSCredentials from './lib/config/awsConfig';
+
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config();
+}
+
+AWS.config.update(getAWSCredentials())
+
+const app = Consumer.create({
+  queueUrl: process.env.SQS_URL,
+  handleMessage: (function (message, done) {
+    // do some work with `message`
+    console.log(message)
+    done();
+  }),
+  sqs: new AWS.SQS()
+});
+
+app.on('error', function (err) {
+  console.log(err.message);
+});
+
+app.start();
