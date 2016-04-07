54ad16008f712b5ea18b3408ea9df5bb249dc376
ADRDra <alexisHetic>
2016-04-07 15:51:14 +0200

    create an event parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Consumer from 'sqs-consumer';
 import AWS from 'aws-sdk';
 import getAWSCredentials from './lib/config/awsConfig';
+import eventParse from './lib/eventParse';
 
 if (process.env.NODE_ENV != 'production') {
   require('dotenv').config();
@@ -12,7 +13,9 @@ const app = Consumer.create({
   queueUrl: process.env.SQS_URL,
   handleMessage: (function (message, done) {
     // do some work with `message`
-    console.log(message)
+
+    console.log(eventParse.parse(message))
+
     done();
   }),
   sqs: new AWS.SQS()
